dc81be99115fe88392908d6527f91a99af508fff
khurananick <khurananick@gmail.com>
2020-12-10 11:39:27 -0500

    Update hello-world.js

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,5 +1,5 @@
 exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say('Hello World, this is a test!');
+  twiml.say('Hello World, this is another test!');
   callback(null, twiml);
 };
