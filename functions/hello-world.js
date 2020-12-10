d8a74e3e224bf6e87981eeb109f8c438ed1c433b
khurananick <khurananick@gmail.com>
2020-12-10 11:45:10 -0500

    testing deploy + jest

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,5 +1,5 @@
 exports.handler = function(context, event, callback) {
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say('Hello World, this is another test. And one more time!!');
+  twiml.say('Hello World, testing deploy + jest.');
   callback(null, twiml);
 };
